1387aae5936c338c90eaf941c1ccf6613b9d9bd0
AlphaCodingCode <52750549+AlphaCodingCode@users.noreply.github.com>
2019-07-23 20:39:07 -0400

    Update IMDB-Scraping.py

--- IMDB-Scraping.py
+++ IMDB-Scraping.py
@@ -1,95 +1,47 @@
 
-from bs4 import BeautifulSoup as SOUP 
-import re 
-import requests as HTTP 
+from bs4 import BeautifulSoup as SOUP
+import re
+import requests as HTTP
 
-# Main Function for scraping 
-def main(emotion): 
+# Main Function for scraping
+def main(genre):
 
-	# IMDb Url for Drama genre of 
-	# movie against emotion Sad 
-	if(emotion == "Sad"): 
-		urlhere = 'http://www.imdb.com/search/title?genres=drama&title_type=feature&sort=moviemeter, asc'
 
-	# IMDb Url for Musical genre of 
-	# movie against emotion Disgust 
-	elif(emotion == "Disgust"): 
-		urlhere = 'http://www.imdb.com/search/title?genres=musical&title_type=feature&sort=moviemeter, asc'
+    urlIMDB = "https://www.imdb.com/search/title/?genres="+genre
+    # HTTP request to get the data of
+    # the whole page
+    response = HTTP.get(urlIMDB)
+    data = response.text
 
-	# IMDb Url for Family genre of 
-	# movie against emotion Anger 
-	elif(emotion == "Anger"): 
-		urlhere = 'http://www.imdb.com/search/title?genres=family&title_type=feature&sort=moviemeter, asc'
+    # Parsing the data using
+    # BeautifulSoup
+    soup = SOUP(data, "lxml")
 
-	# IMDb Url for Thriller genre of 
-	# movie against emotion Anticipation 
-	elif(emotion == "Anticipation"): 
-		urlhere = 'http://www.imdb.com/search/title?genres=thriller&title_type=feature&sort=moviemeter, asc'
+    # Extract movie titles from the
+    # data using regex
+    title = soup.find_all("a", attrs = {"href" : re.compile(r'\/title\/tt+\d*\/')})
+    return title
 
-	# IMDb Url for Sport genre of 
-	# movie against emotion Fear 
-	elif(emotion == "Fear"): 
-		urlhere = 'http://www.imdb.com/search/title?genres=sport&title_type=feature&sort=moviemeter, asc'
+# Driver Function
+if __name__ == '__main__':
 
-	# IMDb Url for Thriller genre of 
-	# movie against emotion Enjoyment 
-	elif(emotion == "Enjoyment"): 
-		urlhere = 'http://www.imdb.com/search/title?genres=thriller&title_type=feature&sort=moviemeter, asc'
+    genre = raw_input("Enter a genre: ")
+    a = main(genre)
+    count = 0
+    genre=genre.lower()
 
-	# IMDb Url for Western genre of 
-	# movie against emotion Trust 
-	elif(emotion == "Trust"): 
-		urlhere = 'http://www.imdb.com/search/title?genres=western&title_type=feature&sort=moviemeter, asc'
 
-	# IMDb Url for Film_noir genre of 
-	# movie against emotion Surprise 
-	elif(emotion == "Surprise"): 
-		urlhere = 'http://www.imdb.com/search/title?genres=film_noir&title_type=feature&sort=moviemeter, asc'
+    for i in a:
 
-	# HTTP request to get the data of 
-	# the whole page 
-	response = HTTP.get(urlhere) 
-	data = response.text 
+        # Splitting each line of the
+        # IMDb data to scrape movies
+        tmp = str(i).split('</a>')
+        if(len(tmp) == 2):
+            tmp =tmp[0].split ('>')
+            if(len(tmp) == 2):
+                print(tmp[1])
+                count += 1
+        if(count > 10):
+            break
 
-	# Parsing the data using 
-	# BeautifulSoup 
-	soup = SOUP(data, "lxml") 
-
-	# Extract movie titles from the 
-	# data using regex 
-	title = soup.find_all("a", attrs = {"href" : re.compile(r'\/title\/tt+\d*\/')}) 
-	return title 
-
-# Driver Function 
-if __name__ == '__main__': 
-
-	emotion = raw_input("Enter the emotion: ") 
-	a = main(emotion) 
-	count = 0
-
-	if(emotion == "Disgust" or emotion == "Anger"
-						or emotion=="Surprise"): 
-
-		for i in a: 
-
-			# Splitting each line of the 
-			# IMDb data to scrape movies 
-			tmp = str(i).split('>;') 
-
-			if(len(tmp) == 3): 
-				print(tmp[1][:-3]) 
-
-			if(count > 13): 
-				break
-			count += 1
-	else: 
-		for i in a: 
-			tmp = str(i).split('>') 
-
-			if(len(tmp) == 3): 
-				print(tmp[1][:-3]) 
-
-			if(count > 11): 
-				break
-			count+=1
 
